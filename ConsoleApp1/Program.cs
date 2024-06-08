86c8e67ca0885cd8cf8cf61c0a7b4d7bd4f0d792
indaleen <inquinsayas@gmail.com>
2024-06-08 14:51:45 +0800

    Initial commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace GettingInput
+{
+    class Program
+    {
+        static void Main()
+        {
+            int count = 0;
+            while (count + 2 <= 10)
+            {
+                Console.WriteLine('a');
+                count++;
+            }
+            Console.WriteLine("!");
+        }
+    }
+}
